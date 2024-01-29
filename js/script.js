ffff14015a8f4e798d37260f578cbd101669c061
Tobias Madaj <tobim1996@outlook.de>
2024-01-29 01:11:27 +0100

    change light mode to dark mode

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 /**
- * @copyright codewithsadee 2023
- * @author Sadee <codewithsadee24@gmail.com>
+ * @author Tobias Madaj
  */
 
 "use strict";
@@ -12,17 +11,17 @@
 
 const /** {NodeElement} */ $themeBtn = document.querySelector("[data-theme-btn]");
 const /** {NodeElement} */ $HTML = document.documentElement;
-let /** {Boolean | String} */ isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+let /** {Boolean | String} */ isLight = window.matchMedia("(prefers-color-scheme: light)").matches;
 
 if (sessionStorage.getItem("theme")) {
   $HTML.dataset.theme = sessionStorage.getItem("theme");
 } else {
-  $HTML.dataset.theme = isDark ? "dark" : "light";
+  $HTML.dataset.theme = isLight ? "dark" : "light";
 }
 
 const changeTheme = () => {
 
-  $HTML.dataset.theme = sessionStorage.getItem("theme") === "light" ? "dark" : "light";
+  $HTML.dataset.theme = sessionStorage.getItem("theme") === "dark" ? "light" : "dark";
   sessionStorage.setItem("theme", $HTML.dataset.theme);
 
 }
